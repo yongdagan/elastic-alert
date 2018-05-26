fe229de76efdb2e685f5a13e7775ec00610a2ef8
yongdagan <yongdagan@gmail.com>
2018-05-26 13:16:59 +0800

    add log

--- src/main/java/com/gmail/yongdagan/elkalert/controller/TestController.java
+++ src/main/java/com/gmail/yongdagan/elkalert/controller/TestController.java
@@ -3,6 +3,8 @@ package com.gmail.yongdagan.elkalert.controller;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 	
+	private static Logger logger = LoggerFactory.getLogger(TestController.class);
+	
 	@RequestMapping(value = "/iptest", method=RequestMethod.GET)
 	public String iptest() {
 		try {
-			return InetAddress.getLocalHost().getHostAddress();
+			String ip = InetAddress.getLocalHost().getHostAddress();
+			logger.info("请求获取本机IP：{}", ip);
+			return ip;
 		} catch (UnknownHostException e) {
+			logger.error("请求获取本机IP失败", e);
 			return "error exists";
 		}
 	}
